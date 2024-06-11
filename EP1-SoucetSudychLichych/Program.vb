2fd70e7ef30f4dbeaf48d0eafe27b46cfb05df36
Daniel Suchý <Suchy.Daniel@stud.vassboskovice.cz>
2024-06-11 09:01:15 +0200

    Výpočet

--- EP1-SoucetSudychLichych/Program.vb
+++ EP1-SoucetSudychLichych/Program.vb
@@ -2,6 +2,18 @@ Imports System
 
 Module Program
   Sub Main(args As String())
-    Console.WriteLine("Hello World!")
-  End Sub
+        Dim pocet, cislo, soucet As Integer
+
+
+
+        Console.WriteLine("Zadej poèet èísel:")
+        pocet = Console.ReadLine
+        Do Until pocet = 0
+            Console.WriteLine("Napiš èíslo:")
+            cislo = Console.ReadLine
+            soucet += cislo
+            pocet = pocet - 1
+        Loop
+        Console.WriteLine($"Souèet èísel je: {soucet}")
+    End Sub
 End Module
